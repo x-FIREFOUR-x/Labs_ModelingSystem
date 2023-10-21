7220a2b427b1048dea37bcb5c4a5bad24a1e1994
Alex <olexandrpasalsky@gmail.com>
2023-10-21 13:25:10 +0300

    nextElement Selector, next element depend that type item depend

---
+++ Lab3/Lab3/Model/NextElementSelector/NextElementItemTypeSelector.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+using Lab3.Model.Elements;
+using Lab3.Model.Queue;
+
+namespace Lab3.Model.NextElementSelector
+{
+    public class NextElementItemTypeSelector<T> : NextElementSelector<T> where T : DefaultQueueItem
+    {
+        public NextElementItemTypeSelector(List<(Element<T>, double)> nextElements)
+           : base(nextElements)
+        {
+        }
+
+        public override Element<T> GetNextElement(DefaultQueueItem item)
+        {
+            if(item is Patient patient)
+            {
+                return _nextElements.FirstOrDefault(c => (int)(c.Item2) == patient.TypePatient).Item1;
+            }  
+
+            return null;
+        }
+    }
+}

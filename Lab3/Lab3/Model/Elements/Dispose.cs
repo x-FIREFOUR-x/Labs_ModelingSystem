3d25c68e4db393147ae7f22943286dbd5ad8f6a8
Alex <olexandrpasalsky@gmail.com>
2023-10-21 13:25:28 +0300

    Dispose

---
+++ Lab3/Lab3/Model/Elements/Dispose.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+using Lab3.Model.DelayGenerator;
+using Lab3.Model.Queue;
+
+
+namespace Lab3.Model.Elements
+{
+    public class Dispose<T> : Element<T> where T : DefaultQueueItem
+    {
+        public Dispose(string name)
+            :base(name)
+        {
+            _currentTime = 0;
+            SetNextTime(Double.PositiveInfinity);
+        }
+
+        public override void StartService(T item) 
+        {
+            if(item is Patient patient)
+            {
+                patient.Finish(_currentTime);
+            }
+            _countProcessed++;
+        }
+
+        public override void FinishService()
+        {
+        }
+
+        public override void PrintStats(bool finalStats)
+        {
+            base.PrintStats(finalStats);
+            Console.WriteLine($"\t\tFinish items: {_countProcessed}");
+        }
+    }
+}

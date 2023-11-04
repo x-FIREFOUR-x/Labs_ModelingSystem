7e3ca0cecf7c6c8a4e0e4c303a13e16e9ea6bf3f
Alex <olexandrpasalsky@gmail.com>
2023-11-04 15:14:14 +0200

    ModelsFactory

---
+++ Lab4/Lab4/ModelsFactory.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections.Generic;
+
+using Lab4.Model;
+using Lab4.Model.DelayGenerator;
+using Lab4.Model.Elements;
+using Lab4.Model.NextElementSelector;
+using Lab4.Model.Queue;
+
+namespace Lab4
+{
+    public class ModelsFactory
+    {
+        public static Model<DefaultQueueItem> CreateSequentialModel(int countProcesses)
+        {
+            List<Element<DefaultQueueItem>> elements = new();
+
+            Create<DefaultQueueItem> create = new Create<DefaultQueueItem>("Create", new ExponentialDelayGenerator(0.5));
+            elements.Add(create);
+
+            Element<DefaultQueueItem> prevElement = create;
+            for (int i = 0; i < countProcesses; i++)
+            {
+                Process<DefaultQueueItem> process = new Process<DefaultQueueItem>((i + 1).ToString(), 2, new List<Element<DefaultQueueItem>> {
+                    new SimpleProcessor<DefaultQueueItem>((i+1).ToString(), new ExponentialDelayGenerator(0.5))}
+                );
+
+                prevElement.NextElementSelector = new NextElementProbabilitySelector<DefaultQueueItem>(
+                    new List<(Element<DefaultQueueItem>, double)> { (process, 1.0) }
+                );
+
+                elements.Add(process);
+                prevElement = process;
+            }
+            //prevElement.NextElementSelector
+
+            return new Model<DefaultQueueItem>(elements);
+        }
+
+        public static Model<DefaultQueueItem> CreateBranchesModel(int countBranches, int countProcess)
+        {
+            List<Element<DefaultQueueItem>> elements = new();
+
+            Create<DefaultQueueItem> create = new Create<DefaultQueueItem>("Create", new ExponentialDelayGenerator(0.5));
+            elements.Add(create);
+
+            List<(Element<DefaultQueueItem>, double)> startBranchesElements = new List<(Element<DefaultQueueItem>, double)>();
+
+            for (int i = 0; i < countBranches; i++)
+            {
+                Element<DefaultQueueItem> prevElement = null;
+                for (int j = 0; j < countProcess; j++)
+                {
+                    string name = (i + 1).ToString() + "-" + (j + 1).ToString();
+                    Process<DefaultQueueItem> process = new Process<DefaultQueueItem>(name, 2, new List<Element<DefaultQueueItem>> {
+                        new SimpleProcessor<DefaultQueueItem>(name, new ExponentialDelayGenerator(0.5))}
+                    );
+
+                    if (j != 0)
+                    {
+                        prevElement.NextElementSelector = new NextElementProbabilitySelector<DefaultQueueItem>(
+                            new List<(Element<DefaultQueueItem>, double)> { (process, 1.0) }
+                        );
+                    }
+                    else
+                    {
+                        startBranchesElements.Add((process, 1.0 / countBranches));
+                    }
+
+                    elements.Add(process);
+                    prevElement = process;
+                }
+                //prevElement.NextElementSelector
+            }
+            create.NextElementSelector = new NextElementProbabilitySelector<DefaultQueueItem>(startBranchesElements);
+
+
+            return new Model<DefaultQueueItem>(elements);
+        }
+    }
+}
--- Lab4/Lab4/Program.cs
+++ Lab4/Lab4/Program.cs
@@ -1,10 +1,4 @@
-﻿using System.Collections.Generic;
-using System;
-
-using Lab4.Model;
-using Lab4.Model.DelayGenerator;
-using Lab4.Model.Elements;
-using Lab4.Model.NextElementSelector;
+﻿using Lab4.Model;
 using Lab4.Model.Queue;
 
 namespace Lab4
@@ -13,78 +7,11 @@ namespace Lab4
     {
         public static void Main(string[] args)
         {
-            Model<DefaultQueueItem> model = createSequentialModel(20);
+            Model<DefaultQueueItem> model = ModelsFactory.CreateSequentialModel(20);
             model.Simulation(100, true);
 
-            //Model < DefaultQueueItem > model = createBranchesModel(5, 4);
+            //Model < DefaultQueueItem > model = ModelsFactory.CreateBranchesModel(5, 4);
             //model.Simulation(100, true);
-        }
-
-        private static Model<DefaultQueueItem> createSequentialModel(int countProcesses)
-        {
-            List<Element<DefaultQueueItem>> elements = new();
-
-            Create<DefaultQueueItem> create = new Create<DefaultQueueItem>("Create", new ExponentialDelayGenerator(0.5));
-            elements.Add(create);
-
-            Element<DefaultQueueItem> prevElement = create;
-            for (int i = 0; i < countProcesses; i++)
-            {
-                Process<DefaultQueueItem> process = new Process<DefaultQueueItem>((i + 1).ToString(), 2, new List<Element<DefaultQueueItem>> {
-                    new SimpleProcessor<DefaultQueueItem>((i+1).ToString(), new ExponentialDelayGenerator(0.5))}
-                );
-
-                prevElement.NextElementSelector = new NextElementProbabilitySelector<DefaultQueueItem>(
-                    new List<(Element<DefaultQueueItem>, double)> { (process, 1.0) }
-                );
-
-                elements.Add(process);
-                prevElement = process;
-            }
-            //prevElement.NextElementSelector
-
-            return new Model<DefaultQueueItem>(elements);
-        }
-
-        private static Model<DefaultQueueItem> createBranchesModel(int countBranches, int countProcess)
-        {
-            List<Element<DefaultQueueItem>> elements = new();
-
-            Create<DefaultQueueItem> create = new Create<DefaultQueueItem>("Create", new ExponentialDelayGenerator(0.5));
-            elements.Add(create);
-
-            List<(Element<DefaultQueueItem>, double)> startBranchesElements = new List<(Element<DefaultQueueItem>, double)>();
-
-            for (int i = 0; i < countBranches; i++)
-            {
-                Element<DefaultQueueItem> prevElement = null;
-                for (int j = 0; j < countProcess; j++)
-                {
-                    string name = (i + 1).ToString() + "-" + (j + 1).ToString();
-                    Process<DefaultQueueItem> process = new Process<DefaultQueueItem>(name, 2, new List<Element<DefaultQueueItem>> {
-                        new SimpleProcessor<DefaultQueueItem>(name, new ExponentialDelayGenerator(0.5))}
-                    );
-
-                    if(j != 0)
-                    {
-                        prevElement.NextElementSelector = new NextElementProbabilitySelector<DefaultQueueItem>(
-                            new List<(Element<DefaultQueueItem>, double)> { (process, 1.0) }
-                        );
-                    }
-                    else
-                    {
-                        startBranchesElements.Add((process, 1.0 / countBranches));
-                    }
-                    
-                    elements.Add(process);
-                    prevElement = process;
-                }
-                //prevElement.NextElementSelector
-            }
-            create.NextElementSelector = new NextElementProbabilitySelector<DefaultQueueItem>(startBranchesElements);
-
-
-            return new Model<DefaultQueueItem>(elements);
         }
     }
 }
